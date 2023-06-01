389b144a64dc7936acbbdf7179d3ad86368f44c8
naga <naga.dereddy@gmail.com>
2023-06-01 20:39:46 +0530

    Method reference Example

--- validation/src/main/java/com/naga/validation/service/MethodReferenceExample.java
+++ validation/src/main/java/com/naga/validation/service/MethodReferenceExample.java
@@ -1,2 +1,17 @@
-package com.naga.validation.service;public class MethodReferenceExample {
+package com.naga.validation.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MethodReferenceExample {
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
 }
